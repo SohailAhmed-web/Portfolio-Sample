ef971300eba9216bfe8645fc2d809e42a92e81d0
SohailAhmed-web <sohail.bscsf19@iba-suk.edu.pk>
2023-10-09 12:28:12 +0500

    updated0

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// JavaScript for smooth scrolling to section links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+function set_background() {
+    const paragraph = document.querySelector('p');
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+    paragraph.style.backgroundColor = 'lightblue';
+    
+}
